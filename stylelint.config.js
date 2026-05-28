75b3edefb971469f4e64ab421b285da69e9c644b
Tonyトニイ <tonngu137@gmail.com>
2026-05-28 08:10:20 +0200

    chore: relax stylelint rules for scss variables and custom property

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,4 +1,8 @@
 /** @type {import("stylelint").Config} */
 export default {
-	extends: ['stylelint-config-standard-scss']
+	extends: ['stylelint-config-standard-scss'],
+	rules: {
+		'scss/dollar-variable-pattern': '^_?[a-z][a-z0-9-]*$',
+		'custom-property-pattern': null
+	}
 };
