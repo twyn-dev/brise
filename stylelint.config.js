f6695fdc2c58f9a2905d9b18178e0748d7058600
Tonyトニイ <tonngu137@gmail.com>
2026-05-29 04:53:50 +0200

    chore: tighten custom property naming pattern

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,7 +2,11 @@
 export default {
 	extends: ['stylelint-config-standard-scss'],
 	rules: {
-		'scss/dollar-variable-pattern': '^_?[a-z][a-z0-9-]*$',
-		'custom-property-pattern': null
+		'scss/dollar-variable-pattern': '^([_-]?[a-z][a-z0-9]*)(-[a-z0-9]+)*$',
+
+		// Stylelint cannot fully validate SCSS-interpolated custom property names like
+		// --#{namespace-prefix()}container-padding. It only sees "#{namespace-prefix",
+		// so this rule enforces the required namespace function fragment.
+		'custom-property-pattern': '^#\\{namespace-prefix$'
 	}
 };
